0272e8fb6e6b4c3a9821cc0a5417accec8373426
Oyebanjo Adebola .O <oyebanjoadebola8@gmail.com>
2023-12-14 11:10:37 -0800

    String

---
+++ string.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+
+/**
+ * str_len - Returns the strength of a string.
+ * @str: Pointer to string length.
+ *
+ * Return: Integer value.
+ */
+int str_len(char *str)
+{
+	int indx = 0;
+
+	if (!str)
+		return (0);
+
+	while (*str++)
+		indx++;
+	return (indx);
+}
+
+/**
+ * str_cmp - Compares two strings lexicographically.
+ * @str1: First string.
+ * @str2: Second string.
+ * Return: Positive (str1 > str2), negative
+ * (str1 < str2), zero (str1 == str2)
+ */
+int str_cmp(char *str1, char *str2)
+{
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+	if (*str1 == *str2)
+		return (0);
+	else
+		return (*str1 < *str2 ? -1 : 1);
+}
+
+
+/**
+ * start_with - checks code.
+ * @hstk: String to search.
+ * @ndle: Substring to search.
+ * Return: Char value or NULL
+ */
+char *start_with(const char *hstk, const char *ndle)
+{
+	while (*ndle)
+		if (*ndle++ != *hstk++)
+			return (NULL);
+	return ((char *)hstk);
+}
+
